6c86b4bf32d5c623ccbd7254284b1a046462a42d
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2024-08-26 00:48:26 +0300

    Array Class file added

---
+++ UserStack_Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserStack
+{
+    internal class UserStack_Array
+    {
+        //Variable Declarations.
+        static int maxSize;
+        int top;
+        int[] stack;
+
+        //constructors.
+        public UserStack_Array(int size = 10)
+        {
+            maxSize = size;
+            stack = new int[maxSize];
+            top = -1;
+        }
+
+        //methods
+        public bool IsEmpty()
+        {
+            return top < 0;
+        }
+    }
+}
