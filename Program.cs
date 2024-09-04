e44b6418be8bb7fd522748ec4731bee77f77ec4d
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2024-09-04 23:35:21 +0300

    Updated menu and the main program

--- Program.cs
+++ Program.cs
@@ -9,19 +9,19 @@ internal class Program
         Console.WriteLine("            Stack Operations Menu        ");
         Console.WriteLine("=========================================");
         Console.WriteLine("Please choose an option:");
-        Console.WriteLine("1. Push - Stores an object on top of the stack");
-        Console.WriteLine("2. Pop  - Remove the top element from the stack");
-        Console.WriteLine("3. Peek - View the top element of the stack");
-        Console.WriteLine("4. Print- Display all elements in the stack");
-        Console.WriteLine("5. Clear- Remove all elements from the stack");
-        Console.WriteLine("6. Exit - Exit the application");
+        Console.WriteLine("[1] Push - Stores an object on top of the stack");
+        Console.WriteLine("[2] Pop  - Remove the top element from the stack");
+        Console.WriteLine("[3] Peek - View the top element of the stack");
+        Console.WriteLine("[4] Print- Display all elements in the stack");
+        Console.WriteLine("[5] Clear- Remove all elements from the stack");
+        Console.WriteLine("[0] Exit - Exit the application");
         Console.WriteLine("=========================================");
-        Console.Write("Enter your choice (1-6): ");
+        Console.Write("Enter your choice [1-5] or [0] to exit: ");
     }
     private static void Main(string[] args)
     {
         bool exit = false;
-        UserStack_Array<int> stack = new UserStack_Array<int>(3);        
+        UserStack_Array stack = new UserStack_Array(3);        
 
         while (!exit)
         {
@@ -32,8 +32,29 @@ internal class Program
             {
                 case "1":
                     Console.Write("Enter value to push: ");
-                    int value = int.Parse(Console.ReadLine());
-                    stack.Push(value);
+                    var value = Console.ReadLine();
+
+                    if (int.TryParse(value, out int intValue))
+                    {
+                        stack.Push(intValue);
+                    }
+                    else if (double.TryParse(value, out double doubleResult))
+                    {
+                        stack.Push(doubleResult);
+                    }
+                    else if (char.TryParse(value, out char charResult))
+                    {
+                        stack.Push(charResult);
+                    }
+                    else if (bool.TryParse(value, out bool boolResult))
+                    {
+                        stack.Push(boolResult);
+                    }
+                    else
+                    {
+                        stack.Push(value); //string
+                    }
+
                     break;
 
                 case "2":
@@ -52,14 +73,14 @@ internal class Program
                     stack.Clear();
                     break;
 
-                case "6":
+                case "0":
                     exit = true;
                     Console.WriteLine("Bye Bye.");
                     break;
 
                 default:
                     //Console.Clear();
-                    Console.WriteLine("\aInvalid option!. Please enter a number between 1 and 6.");                    
+                    Console.WriteLine("\aInvalid option!. Please enter a number between 1 and 5 or 0 to exit the progam.");                    
                     break;
             }
 
